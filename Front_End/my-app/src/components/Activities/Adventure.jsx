879ad505296d853abeee31f367747c9a09ccbe06
Midhat <midhatbaba424@gmail.com>
2024-06-06 12:16:39 +0530

    fixed

--- Front_End/my-app/src/components/Activities/Adventure.jsx
+++ Front_End/my-app/src/components/Activities/Adventure.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Card from "../CardsComponent/Card"; 
+// import Card from "../CardsComponent/Card"; 
 import Content from "../CardsComponent/Content";
 import { Carousel } from '@mantine/carousel';
 import { Card, Image, Text, useMantineTheme } from '@mantine/core';
@@ -17,7 +17,7 @@ function Adventure(){
           slidesToScroll={1}
           breakpoints={[
           { maxWidth: 'md', slideSize: '50%' },
-          { maxWidth: 'sm', slideSize: '100%' },Card,
+          { maxWidth: 'sm', slideSize: '100%' },
         ]}
       >
      {Content.slice(4, 12).map((item) => (
--- Front_End/my-app/src/components/App.jsx
+++ Front_End/my-app/src/components/App.jsx
@@ -9,6 +9,7 @@ function App() {
           <Nav /> 
           <Hero /> 
           <Destination />
+
           <Adventure />
         </div>
     );
