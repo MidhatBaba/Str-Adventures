09d7692d0338beceaba560c39af423a7f27e7e85
Abrar Ajaz Wabni <abhaywani114@gmail.com>
2024-06-13 14:51:20 +0530

    Code fix

--- Front_End/my-app/src/App.jsx
+++ Front_End/my-app/src/App.jsx
@@ -1,38 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router,Switch, Route, Link } from "react-router-dom";
-import Nav from "./components/Nav/Nav.jsx";
-import Hero from "./components/hero/hero.jsx";
-import Destination from "./components/Destination/Destination.jsx";
-import Adventure from "./components/Activities/Adventure.jsx";
-import Trek from "./components/Treks/trek.jsx";
-import Footer from "./components/Footer/Footer.jsx";
-import About_Us from "./components/About/About_Us.jsx";
-import Tryme from "./components/PageComponent/tryme.jsx";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom"
+import IndexPage from "./pages/Index.jsx"
 function App() {
-    return(
-      
-      <Router>
-        <div>
-          <Nav />
-          <Hero /> 
-          <Destination >
-            <ul>
-              <li>
-                <Link to="/Tryme">tryme</Link>
-              </li>
-            </ul>
-          </Destination>
-          <Switch>
-            <Route path="/Tryme"> <Tryme />  </Route>
-            </Switch>            
-        
-          <Trek />
-          <Adventure />
-          <About_Us />
-          <Footer />
-          {/* <Tryme /> */}
-        </div>
-        </Router>
-    );
+  return(
+    <HashRouter  basename="/">
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+      </Routes>
+    </HashRouter> 
+  );
 }
 export default App;
