82a53cb1ac5d91a207d87eeb03b089056fe441d1
Midhat <midhatbaba424@gmail.com>
2024-06-13 16:17:15 +0530

    Addedfiles

--- Front_End/my-app/src/components/destination/Destination.jsx
+++ Front_End/my-app/src/components/destination/Destination.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "../CardsComponent/Card";
 import Content from "../CardsComponent/Content";
+import { Link } from "react-router-dom";
 import "./Destination.css";
 function Destination(){
     const selectCards= Content.slice(0,4);
@@ -8,7 +9,10 @@ function Destination(){
         return <Card 
         key={cont.id}
         img={cont.image}
-        name={cont.name}/>
+        name={cont.name}>
+          <Link to="/tryme" className="card-link">Learn More</Link>
+        </Card>    
+          
     } 
     return(
         <div>
